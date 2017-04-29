d46f290c84013324d253991adc7bcd348ef2d948
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2017-04-29 23:30:12 +0900

    add note row to timetable

--- components/TimeTable.js
+++ components/TimeTable.js
@@ -2,8 +2,9 @@ import React from 'react';
 import {
   Table, TableBody, TableHeaderColumn, TableRow, TableRowColumn,
 } from 'material-ui/Table';
+import ActionInfo from 'material-ui/svg-icons/action/info-outline';
 import NavigationMoreVert from 'material-ui/svg-icons/navigation/more-vert';
-import { white } from 'material-ui/styles/colors';
+import { white, grey500 } from 'material-ui/styles/colors';
 import { premiumBlack, silver } from '../themes/colors';
 
 const TimeTable = ({ data }) => (
@@ -14,22 +15,34 @@ const TimeTable = ({ data }) => (
   </Table>
 );
 
-const RowIterator = ({ hour, minutes }) => (
-  hour === 18 ? [
-    <TableRow>
-      <TableRowColumn colSpan="13" style={styles.dividerColumn}>
-        <NavigationMoreVert color={silver} />
-      </TableRowColumn>
-    </TableRow>,
-    buildRow({ hour, minutes }),
-  ] : buildRow({ hour, minutes })
-);
+const RowIterator = ({ hour, minutes, divider, note }) => {
+  if (divider != null) { return buildDivider(); }
+  if (note != null) { return buildNote(note); }
+  return buildRow({ hour, minutes });
+};
 const buildRow = ({ hour, minutes }) => (
   <TableRow key={hour} style={styles.row}>
     <TableHeaderColumn style={{ ...styles.column, ...styles.hourColumn }}>
       {format(hour)}
     </TableHeaderColumn>
     {fill(minutes).map(makeMinutesRowIterator(hour))}
+  </TableRow>
+);
+const buildDivider = () => (
+  <TableRow key="divider">
+    <TableRowColumn colSpan="13" style={styles.dividerColumn}>
+      <NavigationMoreVert color={silver} />
+    </TableRowColumn>
+  </TableRow>
+);
+const buildNote = note => (
+  <TableRow displayBorder={false} key={`note-${note}`} style={styles.row}>
+    <TableRowColumn colSpan="13" style={styles.noteColumn}>
+      <span style={styles.icon}>
+        <ActionInfo color={styles.noteColumn.color} style={styles.svg} />
+      </span>
+      {note}
+    </TableRowColumn>
   </TableRow>
 );
 const makeMinutesRowIterator = hour => (minute, i) => (
@@ -55,6 +68,13 @@ const styles = {
     backgroundColor: white,
     textAlign: 'center',
   },
+  noteColumn: {
+    backgroundColor: white,
+    height: 13,
+    paddingLeft: '0.5em',
+    fontSize: 11,
+    color: grey500,
+  },
   hourColumn: {
     color: silver,
     backgroundColor: premiumBlack,
@@ -64,6 +84,21 @@ const styles = {
     height: 18,
     paddingLeft: 0,
     paddingRight: 0,
+  },
+  icon: {
+    display: 'inline-flex',
+    alignSelf: 'center',
+    position: 'relative',
+    height: '1.4em',
+    width: '1.4em',
+    marginRight: '0.3em',
+  },
+  svg: {
+    color: 'inherit',
+    height: '1.4em',
+    width: '1.4em',
+    position: 'absolute',
+    bottom: '-0.3em',
   },
 };
 
