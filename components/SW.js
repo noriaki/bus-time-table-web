bbe8b98d2dc971715221ca307c6cc56685beef22
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2017-05-09 16:35:42 +0900

    add tracking banner event (show/install/dismiss)

--- components/SW.js
+++ components/SW.js
@@ -1,10 +1,31 @@
 import { Component } from 'react';
 
+import GA from './GA';
+
 export default class extends Component {
   componentDidMount() {
     if ('serviceWorker' in navigator) {
       navigator.serviceWorker.register('/sw.js');
     }
+    window.addEventListener('beforeinstallprompt', (e) => {
+      GA.event({
+        category: 'Banner',
+        action: 'show',
+        label: 'add-to-homescreen',
+      });
+      e.userChoice.then(result => (
+        result.outcome === 'dismissed' ?
+          GA.event({
+            category: 'Banner',
+            action: 'dismiss',
+            label: 'add-to-homescreen',
+          }) : GA.event({
+            category: 'Banner',
+            action: 'install',
+            label: 'add-to-homescreen',
+          })
+      ));
+    });
   }
 
   render() { return null; }
