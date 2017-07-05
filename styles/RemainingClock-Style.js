baefb1a06cf5b1e5c3c2153d4a1404df3e330cd7
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2017-07-05 14:08:50 +0900

    add style object of RemainingClock

---
+++ styles/RemainingClock-Style.js
@@ -0,0 +1,44 @@
+export const container = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  margin: 10,
+};
+export const boardContainer = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-end',
+  marginRight: 5,
+};
+export const departure = {
+  marginLeft: 5,
+  fontSize: 14,
+  fontWeight: 'bold',
+};
+export const destination = {
+  marginLeft: 5,
+  fontSize: 13,
+};
+export const timer = {
+  fontSize: 25,
+  fontWeight: 'bold',
+  marginLeft: 5,
+};
+export const suffix = {
+  fontSize: 10,
+  fontWeight: 'normal',
+};
+export const notice = {
+  fontSize: 13,
+  textAlign: 'center',
+};
+
+export default {
+  container,
+  boardContainer,
+  departure,
+  destination,
+  timer,
+  suffix,
+  notice,
+};
