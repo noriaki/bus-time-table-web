56d4e10f0da2dc9ff1e6f42f0ed72be0918b47fb
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2017-07-21 10:52:50 +0900

    refactor rendering html of RemainingClock component

--- components/RemainingClock.js
+++ components/RemainingClock.js
@@ -22,21 +22,22 @@ const RemainingClock = ({ remaining, time, dest, inactive, styles }) => {
     const hour = <span className={styles.timer}>{m.format('HH')}</span>;
     const minute = <span className={styles.timer}>{m.format('mm')}</span>;
     const second = <span className={styles.timer}>{m.format('ss')}</span>;
+    const suffix = s => <span className={styles.suffix}>{s}</span>;
     return (
       <div className={styles.container}>
         <div className={styles.boardContainer}>
           <div className={styles.departure}>
-            {time.format('HH')}:{time.format('mm')}
-            <span className={styles.suffix}>発</span>
+            {time.format('HH')}:{time.format('mm')}{suffix('発')}
           </div>
           <div className={styles.destination}>
-            {dest}<span className={styles.suffix}>行</span></div>
+            {dest}{suffix('行')}
+          </div>
         </div>
         <div className={styles.timerContainer}>
           {haveHour ? hour : null}
-          {haveHour ? <span className={styles.suffix}>時間</span> : null}
-          {minute}<span className={styles.suffix}>分</span>
-          {second}<span className={styles.suffix}>秒後</span>
+          {haveHour ? suffix('時間') : null}
+          {minute}{suffix('分')}
+          {second}{suffix('秒後')}
         </div>
       </div>
     );
