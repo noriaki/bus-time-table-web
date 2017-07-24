1743f27e96c9974891dc20972c4804b93d0233c2
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2017-07-25 00:47:21 +0900

    add support for double tap(click) event
    via: https://gist.github.com/MoOx/12726d85a3343d84ee3c

---
+++ libs/isDoubleTouchTap.js
@@ -0,0 +1,20 @@
+const delay = 250;
+let latestTouchTap = {
+  time: 0,
+  target: null,
+};
+
+const isDoubleTouchTap = (event) => {
+  const touchTap = {
+    time: Date.now(),
+    target: event.currentTarget,
+  };
+  const result = (
+    touchTap.target === latestTouchTap.target &&
+      touchTap.time - latestTouchTap.time < delay
+  );
+  latestTouchTap = touchTap;
+  return result;
+};
+
+export default isDoubleTouchTap;
