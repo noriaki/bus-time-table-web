9bd0dc63c4874b08fdce3138f6100ce9f328c2e9
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2017-07-26 11:19:54 +0900

    add plugin to build config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,22 @@
+const { DefinePlugin } = require('webpack');
+const GitRevisionPlugin = require('git-revision-webpack-plugin');
+
 module.exports = {
   webpack(config) {
+    const notProd = process.env.NODE_ENV !== 'production';
+
     if (config.resolve.alias) {
       /* eslint-disable no-param-reassign */
       delete config.resolve.alias.react;
       delete config.resolve.alias['react-dom'];
       /* eslint-enable no-param-reassign */
     }
+
+    const branch = notProd ? new GitRevisionPlugin().branch() : '';
+    config.plugins.push(
+      new DefinePlugin({ BRANCH: JSON.stringify(branch) })
+    );
+
     return config;
   },
 };
