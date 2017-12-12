9e8e7c2570f6de83b165bdcdf9caae89c9448f45
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2017-12-12 19:19:16 +0900

    add JSS support for MUI

--- pages/_document.js
+++ pages/_document.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import Document, { Head, Main, NextScript } from 'next/document';
-import { rewind } from 'react-free-style';
+import JssProvider from 'react-jss/lib/JssProvider';
+import getContext from '../styles/getContext';
 
-const styles = rewind();
-
-export default class extends Document {
+class DocumentContainer extends Document {
   render() {
     return (
       <html lang="ja">
         <Head>
           <meta
             name="viewport"
-            content="width=device-width, initial-scale=1.0" />
+            content="width=device-width, initial-scale=1, user-scalable=no" />
           <link rel="stylesheet" href="/static/normalize.css" />
           <link
             rel="stylesheet"
@@ -21,7 +20,6 @@ export default class extends Document {
             href="//fonts.googleapis.com/earlyaccess/notosansjp.css" />
           <link rel="stylesheet" href="/static/style.css" />
           <link rel="shortcut icon" href="/static/icons/favicon.ico" />
-          {styles.toComponent()}
         </Head>
         <body>
           <Main />
@@ -31,3 +29,42 @@ export default class extends Document {
     );
   }
 }
+DocumentContainer.getInitialProps = ({ renderPage }) => {
+  // Resolution order
+  //
+  // On the server:
+  // 1. page.getInitialProps
+  // 2. document.getInitialProps
+  // 3. page.render
+  // 4. document.render
+  //
+  // On the server with error:
+  // 2. document.getInitialProps
+  // 3. page.render
+  // 4. document.render
+  //
+  // On the client
+  // 1. page.getInitialProps
+  // 3. page.render
+
+  // Get the context to collected side effects.
+  const context = getContext();
+  const page = renderPage(Component => props => (
+    <JssProvider registry={context.sheetsRegistry} jss={context.jss}>
+      <Component {...props} />
+    </JssProvider>
+  ));
+
+  return {
+    ...page,
+    stylesContext: context,
+    styles: (
+      <style
+        id="jss-server-side"
+        // eslint-disable-next-line react/no-danger
+        dangerouslySetInnerHTML={{ __html: context.sheetsRegistry.toString() }} />
+    ),
+  };
+};
+
+export default DocumentContainer;
