9538d06c148698a6bf1ef0d7bdac6130df9d148c
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2017-12-18 19:27:54 +0900

    [WIP] add app title bar component

---
+++ components/AppTitleBar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import IconButton from 'material-ui/IconButton';
+import Menu, { MenuItem } from 'material-ui/Menu';
+import ShareIcon from 'material-ui-icons/Share';
+
+const AppTitleBar = ({ classes, title }) => (
+  <AppBar>
+    <Toolbar>
+      <Typography type="title" color="inherit">{title}</Typography>
+    </Toolbar>
+  </AppBar>
+);
+
+export default AppTitleBar;
