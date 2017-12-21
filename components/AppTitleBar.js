7609ed4935a59b08d84bc6928f2a23d6ec984432
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2017-12-21 17:46:16 +0900

    add share-menu to title-bar

--- components/AppTitleBar.js
+++ components/AppTitleBar.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import NoSSR from 'react-no-ssr';
 import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
-import IconButton from 'material-ui/IconButton';
-import Menu, { MenuItem } from 'material-ui/Menu';
-import ShareIcon from 'material-ui-icons/Share';
+
+import ShareMenu from './ShareMenu';
 
 const AppTitleBar = ({ classes, title }) => (
   <AppBar>
     <Toolbar>
-      <Typography type="title" color="inherit">{title}</Typography>
+      <Typography type="title" color="inherit" style={{ flexGrow: 1 }}>
+        {title}
+      </Typography>
+      <NoSSR>
+        <ShareMenu />
+      </NoSSR>
     </Toolbar>
   </AppBar>
 );
