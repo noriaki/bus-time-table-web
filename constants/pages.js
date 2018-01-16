ca34a9dfda8c77f5b3f46323fd9f99492a63ba02
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2018-01-16 15:05:04 +0900

    add app getting page meta data

--- constants/pages.js
+++ constants/pages.js
@@ -13,6 +13,12 @@ const pages = {
     label: '時刻表',
     description: `${description} 時刻表のページでは、マンション発、東銀座駅発、新橋駅発の各バス停の全時間帯のシャトルバス時刻表を一覧で見ることが出来ます`,
   },
+  '/getapp': {
+    title: `${title} - アプリとしてホーム画面へ追加するには`,
+    appbarTitle: 'アプリをホーム画面へ追加',
+    label: 'アプリ',
+    description: `${description} 急いでいるときにも簡単にすぐ起動できるよう、ホーム画面にアプリとして配置（ダウンロード）することが出来ます`,
+  },
   '/info': {
     title: `${title} - お知らせ・アプリ概要`,
     appbarTitle: 'お知らせ・アプリ概要',
