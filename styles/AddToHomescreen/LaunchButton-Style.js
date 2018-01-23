70ff2309705c048b1f6821ed3d558cc64a63f610
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2018-01-23 16:50:54 +0900

    implement smooth animation. NOTE: need to animate only the first time

--- styles/AddToHomescreen/LaunchButton-Style.js
+++ styles/AddToHomescreen/LaunchButton-Style.js
@@ -1,70 +1,88 @@
-const duration = 2;
-const delay = 4;
+// animation properties
+const duration = 1;
+const delay = 3;
+const ratio = 0.3;
+const firstDuration = duration * ratio;
+const secondDuration = duration * (1 - ratio);
+const animationProp = (name, timing = 'ease') => (
+  `${name} ${secondDuration}s ${delay + firstDuration}s ${timing} forwards`
+);
 
-export const root = (theme) => {
+export default (theme) => {
   const { unit } = theme.spacing;
   const toolbarMinHeight = theme.mixins.toolbar.minHeight;
-  const animationName = 'shrink';
-  return {
-    position: 'fixed',
-    right: 0,
-    bottom: toolbarMinHeight,
-    marginRight: unit,
-    marginBottom: unit,
-    minWidth: unit * 3,
-    display: 'flex',
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-    '@global': {
-      [`@keyframes ${animationName}`]: {
-        from: { width: '100%' },
-        to: { width: unit * 7 },
-      },
-    },
-    animation: `${animationName} ${duration}s ${delay}s ease-out forwards`,
+  const secondaryColor = theme.palette.secondary;
+  const baseRaisedAccent = {
+    backgroundColor: secondaryColor[100],
+    color: secondaryColor[900],
   };
-};
 
-export const raisedAccent = (theme) => {
-  const baseProps = {
-    backgroundColor: theme.palette.secondary[100],
-    color: theme.palette.secondary[900],
-  };
   return ({
-    ...baseProps,
-    '&:hover': { ...baseProps },
+    root: {
+      position: 'fixed',
+      right: 0,
+      bottom: toolbarMinHeight,
+      marginRight: unit,
+      marginBottom: unit,
+      minWidth: unit * 3,
+      display: 'flex',
+      justifyContent: 'flex-end',
+      alignItems: 'center',
+      '@global': {
+        '@keyframes shrinkContainer': {
+          from: { maxWidth: '100vw' },
+          to: { maxWidth: unit * 7 },
+        },
+        '@keyframes roundCorner': {
+          to: { borderRadius: '50%' },
+        },
+        '@keyframes equalPadding': {
+          to: { padding: unit * 2 },
+        },
+        '@keyframes floatingPosition': {
+          to: {
+            marginRight: unit * 2,
+            marginBottom: unit * 2,
+          },
+        },
+      },
+      animation: [
+        animationProp('shrinkContainer'),
+        animationProp('roundCorner', 'ease-in'),
+        animationProp('equalPadding', 'ease-in'),
+        animationProp('floatingPosition', 'ease-in'),
+      ],
+    },
+    raisedAccent: {
+      ...baseRaisedAccent,
+      '&:hover': { ...baseRaisedAccent },
+    },
+    icon: {
+      marginRight: unit,
+      '@global': {
+        '@keyframes spin': {
+          from: { transform: 'rotate(0deg)' },
+          to: { transform: 'rotate(360deg)' },
+        },
+        '@keyframes zeroMarginalize': {
+          to: { marginRight: 0 },
+        },
+      },
+      animation: [
+        `spin ${firstDuration}s ${delay}s`,
+        animationProp('zeroMarginalize'),
+      ],
+    },
+    text: {
+      height: '1em',
+      lineHeight: '1em',
+      overflowX: 'hidden',
+      '@global': {
+        '@keyframes fadeOutItem': {
+          to: { opacity: 0 },
+        },
+      },
+      animation: animationProp('fadeOutItem'),
+    },
   });
 };
-
-export const icon = (theme) => {
-  const { unit } = theme.spacing;
-  return {
-    marginRight: unit,
-    '@global': {
-      '@keyframes spin': {
-        from: { transform: 'rotate(0deg)' },
-        to: { transform: 'rotate(360deg)' },
-      },
-      '@keyframes zeroMarginalize': {
-        to: { marginRight: 0 },
-      },
-    },
-    animation: [
-      `spin ${duration * 0.2}s ${delay}s`,
-      `zeroMarginalize ${duration * 0.8}s ${delay + (duration * 0.2)}s ease forwards`,
-    ],
-  };
-};
-
-export const text = {
-  height: '1em',
-  lineHeight: '1em',
-  overflowX: 'hidden',
-};
-
-export default theme => ({
-  root: root(theme),
-  raisedAccent: raisedAccent(theme),
-  icon: icon(theme),
-  text,
-});
