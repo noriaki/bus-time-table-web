48e8f34a21f3dcd8921f6369fdd46c430345213f
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2018-01-24 16:14:50 +0900

    implement fixes of getting componentName

--- libs/setComponentName.js
+++ libs/setComponentName.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import {
   compose,
-  getDisplayName,
   setStatic,
   setDisplayName,
 } from 'recompose';
+import getOriginalDisplayName from './getOriginalDisplayName';
 
 const setComponentName = name => (BaseComponent) => {
-  const componentName = `${getDisplayName(BaseComponent)}[${name}]`;
+  const componentName = `${getOriginalDisplayName(BaseComponent)}[${name}]`;
   const EnhancedComponent = props => (
     <BaseComponent {...props} />
   );
