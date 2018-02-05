dd568831054b4ceba0d19f58d6635b1328ed0836
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2018-02-05 15:56:01 +0900

    fix #28, resolve cannot propt bunner of `add to homescreen`

--- static/sw.js
+++ static/sw.js
@@ -1,6 +1,7 @@
+/* global: self */
+/* eslint-disable no-restricted-globals */
 /* ServiceWorker. Updated at 2018-02-01T21:23:10+09:00 */
 
+self.addEventListener('fetch', () => {});
 
-
-
-
+/* eslint-enable no-restricted-globals */
