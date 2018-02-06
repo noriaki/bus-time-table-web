ccc7298b8594865de7d96cf97d5dcc6c23e09aad
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2018-02-06 18:21:41 +0900

    add support of ofline google analytics
    [WIP] fix 2-times access required for caching

---
+++ libs/service-worker.js
@@ -0,0 +1,67 @@
+/* global: self */
+/* eslint-disable no-restricted-globals */
+
+// const swScriptPath = 'https://unpkg.com/workbox-sw@2.1.2';
+const swScriptPath = 'https://unpkg.com/workbox-sw@2.1.2/build/importScripts/workbox-sw.prod.v2.1.2.js';
+
+// const gaScriptPath = 'https://unpkg.com/workbox-google-analytics@2.1.1';
+const gaScriptPath = 'https://unpkg.com/workbox-google-analytics@2.1.1/build/importScripts/workbox-google-analytics.prod.v2.1.1.js';
+
+importScripts(swScriptPath, gaScriptPath);
+
+const wb = new self.WorkboxSW({
+  clientsClaim: true,
+  skipWaiting: true,
+});
+
+importScripts(gaScriptPath);
+
+const cacheExpireSecondsShort = 60 * 60 * 24 * 7; // 1-week
+const cacheExpireSecondsLong = 60 * 60 * 24 * 30; // 1-month
+const cacheExpireSecondsVeryLong = 60 * 60 * 24 * 30 * 6; // half-a-year
+
+const regexpWebFonts = /^https?:\/\/fonts\.(?:googleapis|gstatic)\.com\/(.*)/;
+wb.router.registerRoute(
+  regexpWebFonts,
+  wb.strategies.cacheFirst({
+    cacheName: 'web-fonts',
+    cacheExpiration: { maxAgeSeconds: cacheExpireSecondsVeryLong },
+  }),
+  'GET'
+);
+
+const regexpPages = /^https?:\/\/[^/]+\/(timetable|info)?$/;
+wb.router.registerRoute(
+  regexpPages,
+  wb.strategies.staleWhileRevalidate({
+    cacheName: 'pages',
+    cacheExpiration: { maxAgeSeconds: cacheExpireSecondsShort },
+  }),
+  'GET'
+);
+
+const regexpPageScripts = /\/_next\/[0-9a-f-]+\/page\/(index|timetable|info)\.js/;
+wb.router.registerRoute(
+  regexpPageScripts,
+  wb.strategies.staleWhileRevalidate({
+    cacheName: 'page-scripts',
+    cacheExpiration: { maxAgeSeconds: cacheExpireSecondsShort },
+  }),
+  'GET'
+);
+
+const regexpAppShell = /\/_next\/[0-9a-f-]*\/[^/]*\.js$/;
+wb.router.registerRoute(
+  regexpAppShell,
+  wb.strategies.staleWhileRevalidate({
+    cacheName: 'app-shell',
+    cacheExpiration: { maxAgeSeconds: cacheExpireSecondsShort },
+  }),
+  'GET'
+);
+
+wb.precache([]);
+
+self.workbox.googleAnalytics.initialize();
+
+/* eslint-enable no-restricted-globals */
--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const { DefinePlugin } = require('webpack');
 const { join } = require('path');
 const GitRevisionPlugin = require('git-revision-webpack-plugin');
 const WorkboxPlugin = require('workbox-webpack-plugin');
-const moment = require('moment');
 
 module.exports = {
   webpack(config) {
@@ -26,61 +25,12 @@ module.exports = {
     );
 
     // precache by service worker
-    const cacheExpireSecondsShort = Math.floor(
-      moment.utc(0).set({ dates: 7, hours: 24 }).valueOf() / 1000
-    ); // 1-week
-    const cacheExpireSecondsLong = Math.floor(
-      moment.utc(0).set({ month: 1 }).valueOf() / 1000
-    ); // 1-month
-    const cacheExpireSecondsVeryLong = Math.floor(
-      moment.utc(0).set({ months: 6 }).valueOf() / 1000
-    ); // half-a-year
     config.plugins.push(
       new WorkboxPlugin({
         globDirectory: __dirname,
         globPatterns: ['static/**/*.{jpg,png,ico,css,json}'],
-        globIgnores: [
-          'node_modules/**/*',
-          '*workbox-sw.prod.v2.1.2.js',
-          '*sw.js',
-        ],
+        swSrc: join(__dirname, 'libs', 'service-worker.js'),
         swDest: join(config.output.path, 'sw.js'),
-        clientsClaim: true,
-        skipWaiting: true,
-        runtimeCaching: [
-          {
-            urlPattern: /^https?:\/\/fonts\.(?:googleapis|gstatic)\.com\/(.*)/,
-            handler: 'cacheFirst',
-            options: {
-              cacheName: 'web-fonts',
-              cacheExpiration: { maxAgeSeconds: cacheExpireSecondsVeryLong },
-            },
-          },
-          {
-            urlPattern: /^https?:\/\/[^/]+\/(timetable|info)?$/,
-            handler: 'staleWhileRevalidate',
-            options: {
-              cacheName: 'pages',
-              cacheExpiration: { maxAgeSeconds: cacheExpireSecondsShort },
-            },
-          },
-          {
-            urlPattern: /\/_next\/[0-9a-f-]+\/page\/(index|timetable|info)\.js/,
-            handler: 'staleWhileRevalidate',
-            options: {
-              cacheName: 'page-scripts',
-              cacheExpiration: { maxAgeSeconds: cacheExpireSecondsShort },
-            },
-          },
-          {
-            urlPattern: /\/_next\/[0-9a-f-]*\/[^/]*\.js$/,
-            handler: 'staleWhileRevalidate',
-            options: {
-              cacheName: 'app-shell',
-              cacheExpiration: { maxAgeSeconds: cacheExpireSecondsShort },
-            },
-          },
-        ],
       })
     );
 
