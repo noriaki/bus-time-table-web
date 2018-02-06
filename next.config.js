e41409b76d010d175ee2f453aaa094d0b6e9aa2f
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2018-02-06 10:56:00 +0900

    add support precaching static files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const { DefinePlugin } = require('webpack');
+const { join } = require('path');
 const GitRevisionPlugin = require('git-revision-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+const moment = require('moment');
 
 module.exports = {
   webpack(config) {
@@ -11,6 +14,7 @@ module.exports = {
       /* eslint-enable no-param-reassign */
     }
 
+    // display git branch/revision
     const git = new GitRevisionPlugin();
     const branch = git.branch();
     const commit = git.commithash();
@@ -18,6 +22,26 @@ module.exports = {
       new DefinePlugin({
         'process.env.GIT_BRANCH': JSON.stringify(branch),
         'process.env.GIT_COMMIT': JSON.stringify(commit),
+      })
+    );
+
+    // precache by service worker
+    const cacheExpireSeconds = Math.floor(
+      moment.utc(0).set({ dates: 7, hours: 24 }).valueOf() / 1000
+    ); // 1-week
+    config.plugins.push(
+      new WorkboxPlugin({
+        globDirectory: __dirname,
+        globPatterns: ['static/**/*.{jpg,png,ico,css}'],
+        globIgnores: [
+          'node_modules/**/*',
+          '*workbox-sw.prod.v2.1.2.js',
+          '*sw.js',
+        ],
+        ignoreUrlParametersMatching: [/./],
+        swDest: join(config.output.path, 'sw.js'),
+        clientsClaim: true,
+        skipWaiting: true,
       })
     );
 
