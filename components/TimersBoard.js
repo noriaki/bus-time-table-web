b170f947a3f673bb39da3132c1b578313f7e0deb
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2019-06-25 18:30:42 +0900

    add timetable

--- components/TimersBoard.js
+++ components/TimersBoard.js
@@ -1,11 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { Fragment } from 'react';
+
+// material-ui
+import Typography from '@material-ui/core/Typography';
 
 // material-ui icons
 import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
 import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
 
 // containers
-import Clock from '~/containers/ClockContainer';
 import {
   HomeTimetable,
   HigashiGinzaTimetable,
@@ -18,45 +20,43 @@ import GuideBoard from './GuideBoard';
 // styles
 import useStyles from '~/styles/TimersBoard-Style';
 
-const TimersBoard = () => {
-  const clock = Clock.useContainer();
-  // componentDidMount, componentWillUnmount
-  useEffect(() => {
-    clock.start();
-    return () => { clock.stop(); };
-  }, []);
-
+const TimersBoard = ({ clock }) => {
   const classes = useStyles();
 
   return (
-    <article className={classes.container}>
-      <div className={classes.itemFullWidth}>
-        <HomeTimetable.Provider>
-          <GuideBoard clock={clock} Timetable={HomeTimetable} />
-        </HomeTimetable.Provider>
+    <Fragment>
+      <Typography component="h2" variant="h6">
+        発車タイマー
+      </Typography>
+      <div className={classes.container}>
+        <div className={classes.itemFullWidth}>
+          <HomeTimetable.Provider>
+            <GuideBoard clock={clock} Timetable={HomeTimetable} />
+          </HomeTimetable.Provider>
+        </div>
+        <div className={classes.itemRightSide}>
+          <ArrowDownwardIcon className={classes.arrowIcon} />
+        </div>
+        <div className={classes.itemRightSide}>
+          <HigashiGinzaTimetable.Provider>
+            <GuideBoard clock={clock} Timetable={HigashiGinzaTimetable} mini />
+          </HigashiGinzaTimetable.Provider>
+        </div>
+        <div className={classes.itemRightSide}>
+          <ArrowDownwardIcon className={classes.arrowIcon} />
+        </div>
+        <div className={classes.itemFullWidth}>
+          <ShimbashiTimetable.Provider>
+            <GuideBoard clock={clock} Timetable={ShimbashiTimetable} />
+          </ShimbashiTimetable.Provider>
+        </div>
+        <div className={classes.itemLeftSide}>
+          <ArrowUpwardIcon className={classes.arrowIcon} />
+          <ArrowUpwardIcon className={classes.arrowIcon} />
+          <ArrowUpwardIcon className={classes.arrowIcon} />
+        </div>
       </div>
-      <div className={classes.itemRightSide}>
-        <ArrowDownwardIcon className={classes.arrowIcon} />
-      </div>
-      <div className={classes.itemRightSide}>
-        <HigashiGinzaTimetable.Provider>
-          <GuideBoard clock={clock} Timetable={HigashiGinzaTimetable} mini />
-        </HigashiGinzaTimetable.Provider>
-      </div>
-      <div className={classes.itemRightSide}>
-        <ArrowDownwardIcon className={classes.arrowIcon} />
-      </div>
-      <div className={classes.itemFullWidth}>
-        <ShimbashiTimetable.Provider>
-          <GuideBoard clock={clock} Timetable={ShimbashiTimetable} />
-        </ShimbashiTimetable.Provider>
-      </div>
-      <div className={classes.itemLeftSide}>
-        <ArrowUpwardIcon className={classes.arrowIcon} />
-        <ArrowUpwardIcon className={classes.arrowIcon} />
-        <ArrowUpwardIcon className={classes.arrowIcon} />
-      </div>
-    </article>
+    </Fragment>
   );
 };
 
