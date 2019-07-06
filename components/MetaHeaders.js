c907d21a74e69439606d334946ac0393495907b9
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2019-07-06 23:04:22 +0900

    add description of app

--- components/MetaHeaders.js
+++ components/MetaHeaders.js
@@ -1,11 +1,15 @@
 import React, { Fragment } from 'react';
 
+// constants
+import { description } from '~/package.json';
+
 const MetaHeaders = () => (
   <Fragment>
     <title>
       シャトルバス時刻表・発車タイマー
       （中央区晴海のドゥ・トゥール/DEUX TOURS）
     </title>
+    <meta name="description" content={description} />
     <meta name="mobile-web-app-capable" content="yes" />
     <link rel="manifest" href="/static/manifest.json" />
     <meta name="apple-mobile-web-app-capable" content="yes" />
