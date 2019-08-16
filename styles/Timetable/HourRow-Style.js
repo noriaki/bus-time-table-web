cb60b611e54a3bc5e635905ee2eecf3300c5c2ee
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2019-08-16 17:38:07 +0900

    fix changes the object hierarchy to be functionalized in the makeStyles properties

--- styles/Timetable/HourRow-Style.js
+++ styles/Timetable/HourRow-Style.js
@@ -1,19 +1,25 @@
 import { makeStyles } from '@material-ui/styles';
 import { fade, lighten } from '@material-ui/core/styles/colorManipulator';
 
-const useStyles = makeStyles(theme => ({
-  head: {
-    width: '2em',
-    borderRight: `solid 1px ${lighten(fade(theme.palette.divider, 1), 0.88)}`,
-  },
-  column: ({ estimated }) => (estimated ? {
-    backgroundColor: theme.palette.grey[200],
-  } : {}),
-  cellContainer: {
-    display: 'grid',
-    gridTemplateColumns: 'repeat(12, 1fr)',
-    padding: '0 8px',
-  },
-}));
+const useStyles = makeStyles(({ palette }) => {
+  const borderColor = lighten(fade(palette.divider, 1), 0.88);
+
+  return ({
+    head: {
+      width: '2em',
+      borderRight: `solid 1px ${borderColor}`,
+    },
+    column: {
+      backgroundColor: ({ estimated }) => (
+        estimated ? palette.grey[200] : 'inherit'
+      ),
+    },
+    cellContainer: {
+      display: 'grid',
+      gridTemplateColumns: 'repeat(12, 1fr)',
+      padding: '0 8px',
+    },
+  });
+});
 
 export default useStyles;
--- styles/Timetable/MinuteCell-Style.js
+++ styles/Timetable/MinuteCell-Style.js
@@ -1,11 +1,15 @@
 import { makeStyles } from '@material-ui/styles';
 import { lighten } from '@material-ui/core/styles/colorManipulator';
 
-const useStyles = makeStyles(theme => ({
-  root: ({ index, current }) => ({
-    gridColumn: index + 1,
-    backgroundColor: current && lighten(theme.palette.secondary.light, 0.5),
-  }),
-}));
+const useStyles = makeStyles(({ palette }) => {
+  const currentColor = lighten(palette.secondary.light, 0.5);
+
+  return {
+    root: {
+      gridColumn: ({ index }) => index + 1,
+      backgroundColor: ({ current }) => current && currentColor,
+    },
+  };
+});
 
 export default useStyles;
