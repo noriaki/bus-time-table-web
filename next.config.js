e3bf7ca21adf8d91058ca897458b195a5194ab39
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2019-09-10 18:58:38 +0900

    implement workbox plugin in build

--- next.config.js
+++ next.config.js
@@ -1,3 +1,26 @@
+const NextWorkboxWebpackPlugin = require('next-workbox-webpack-plugin');
+const { buildId: customBuildId } = require('./constants/build');
+
 module.exports = {
   target: 'serverless',
+  webpack: (config, {
+    isServer,
+    dev,
+    buildId,
+    config: { distDir },
+  }) => {
+    if (!isServer && !dev) {
+      config.plugins.push(
+        new NextWorkboxWebpackPlugin({
+          importWorkboxFrom: 'cdn',
+          distDir,
+          buildId,
+          swDestRoot: `.next/static/${customBuildId}/pages`,
+          swURLRoot: `_next/static/${customBuildId}/pages`,
+        })
+      );
+    }
+    return config;
+  },
+  generateBuildId: async () => customBuildId,
 };
