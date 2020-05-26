c23ebb167acaa9da2b8f58637c1dc56f6fc96cf0
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2020-05-26 10:08:15 +0900

    refactor separation of the Notice component

--- src/components/Notice/index.js
+++ src/components/Notice/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 // material-ui
 import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
 
 // material-icon
 import NotificationsIcon from '@material-ui/icons/Notifications';
@@ -10,6 +9,12 @@ import NotificationsIcon from '@material-ui/icons/Notifications';
 // styles
 import { makeStyles } from '@material-ui/core/styles';
 import { silver } from '~/contexts/mui/themes/colors';
+
+// hooks
+import useNotice from '~/hooks/useNotice';
+
+// components
+import NoticeContent from './NoticeContent';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -29,20 +34,14 @@ const useStyles = makeStyles((theme) => ({
 
 const Notice = () => {
   const classes = useStyles();
+  const { hasNotice, contents } = useNotice();
+
+  if (hasNotice === false) { return null; }
 
   return (
     <Paper square elevation={0} className={classes.root}>
       <NotificationsIcon className={classes.icon} />
-      <div>
-        <Typography variant="body2" className={classes.text}>
-          4/8
-          <Typography component="span" variant="caption">(水)</Typography>
-          から東京都緊急事態措置の影響で一時運休中です。
-        </Typography>
-        <Typography variant="body2" className={classes.text}>
-          再開時期は未定で、管理組合が行政の見解を待って別途告知予定です。
-        </Typography>
-      </div>
+      <NoticeContent contents={contents} />
     </Paper>
   );
 };
