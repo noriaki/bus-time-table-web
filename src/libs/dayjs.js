5dad8b86d269280a544f6fec16d1615bc51e5ed3
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2021-02-10 20:07:37 +0900

    support isBetween

--- src/libs/dayjs.js
+++ src/libs/dayjs.js
@@ -2,10 +2,12 @@ import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
 import objectSupport from 'dayjs/plugin/objectSupport';
 import duration from 'dayjs/plugin/duration';
+import isBetween from 'dayjs/plugin/isBetween';
 
 dayjs.extend(utc);
 dayjs.extend(objectSupport);
 dayjs.extend(duration);
+dayjs.extend(isBetween);
 
 dayjs.initialize = (obj) => {
   const instance = dayjs.isDayjs(obj) ? obj : dayjs(obj);
