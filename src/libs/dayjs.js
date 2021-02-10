95914bfa6817aae28b173344474fe4f86b8de355
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2021-02-10 17:25:41 +0900

    add duration plugin

--- src/libs/dayjs.js
+++ src/libs/dayjs.js
@@ -1,8 +1,10 @@
 import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
 import objectSupport from 'dayjs/plugin/objectSupport';
+import duration from 'dayjs/plugin/duration';
 
 dayjs.extend(utc);
 dayjs.extend(objectSupport);
+dayjs.extend(duration);
 
 export default dayjs;
