6906fff71a4e993199905a527cb8a77f78abefef
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2021-02-28 01:28:56 +0900

    fix tsc error

--- src/libs/dayjs.ts
+++ src/libs/dayjs.ts
@@ -3,7 +3,7 @@ import utc from 'dayjs/plugin/utc';
 import timezone from 'dayjs/plugin/timezone';
 import objectSupport from 'dayjs/plugin/objectSupport';
 import toObject from 'dayjs/plugin/toObject';
-import duration, { Duration } from 'dayjs/plugin/duration';
+import duration from 'dayjs/plugin/duration';
 import isBetween from 'dayjs/plugin/isBetween';
 
 dayjs.extend(utc);
@@ -14,7 +14,8 @@ dayjs.extend(duration);
 dayjs.extend(isBetween);
 
 // config
-dayjs.tz.setDefault('Asia/Tokyo');
+const defaultTZ = 'Asia/Tokyo';
+dayjs.tz.setDefault(defaultTZ);
 
 // constants
 export const SUN = 0 as const;
@@ -29,12 +30,19 @@ export const SAT = 6 as const;
 export const createDayjs = (
   obj?: ConfigType | Record<string, unknown>
 ): Dayjs => {
-  const instance = dayjs.isDayjs(obj) ? obj : dayjs.tz(obj);
+  if (dayjs.isDayjs(obj)) {
+    return obj;
+  }
+  if (obj === undefined) {
+    return dayjs().tz(defaultTZ);
+  }
+  const instance = dayjs.tz(obj as ConfigType);
   if (!instance.isValid()) {
     throw new Error('invalid Dayjs');
   }
   return instance;
 };
 
-export { Dayjs, Duration };
+export type { Dayjs };
+export type { Duration } from 'dayjs/plugin/duration';
 export default dayjs;
