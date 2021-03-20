3b605039d4e00d0049b358cf34a204001b556591
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2021-03-20 15:04:32 +0900

    add getStaticProps

--- src/pages/beta.tsx
+++ src/pages/beta.tsx
@@ -1,7 +1,36 @@
+import { GetStaticProps, NextPage } from 'next';
 import React from 'react';
 
-const BetaPage = () => {
+import Timetable from '~/models/Timetable';
+import TimetableDriver from '~/drivers/TimetableDriver';
+import TimetableRepository from '~/repositories/TimetableRepository';
+import TimetableUseCase from '~/usecases/TimetableUseCase';
+
+type Props = {
+  // buildId: string;
+  // usecase: TimetableUseCase;
+  timetables: Timetable[];
+};
+type NextPageWithProps = NextPage<Props>;
+type GetStaticPropsWithProps = GetStaticProps<Props>;
+
+const BetaPage: NextPageWithProps = ({ timetables }) => {
   return <div>Beta</div>;
+};
+
+export const getStaticProps: GetStaticPropsWithProps = async (context) => {
+  const repository = new TimetableRepository(new TimetableDriver());
+  const usecase = new TimetableUseCase(repository);
+  const timetables = await usecase.fetchAll();
+  // const { VERCEL_GIT_COMMIT_SHA } = process.env;
+
+  return {
+    props: {
+      // buildId: VERCEL_GIT_COMMIT_SHA || 'development',
+      // usecase,
+      timetables,
+    },
+  };
 };
 
 export default BetaPage;
