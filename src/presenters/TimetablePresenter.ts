ad64a6b30b63bc1dfd7c86e87e67997f66a42913
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2021-04-20 20:22:48 +0900

    add presenter of timetables

---
+++ src/presenters/TimetablePresenter.ts
@@ -0,0 +1,20 @@
+import Timetable, { StationId, OperationalDayId } from '~/models/Timetable';
+
+export type GroupedTimetables = {
+  [station in StationId]?: Timetable[];
+};
+
+export const groupByStation = (timetables: Timetable[]): GroupedTimetables =>
+  timetables.reduce<GroupedTimetables>((ret, timetable) => {
+    const station = timetable.station;
+    if (isStationId(station)) {
+      if (ret[station] === undefined) {
+        ret[station] = [];
+      }
+      ret[station]?.push(timetable);
+    }
+    return ret;
+  }, {});
+
+const isStationId = (station: unknown): station is StationId =>
+  Timetable.allStationIds().includes(station as StationId);
