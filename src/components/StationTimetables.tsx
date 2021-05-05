801dc7564aab980ee2e6674cb69079973b36d3d4
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2021-05-06 00:04:54 +0900

    add tab

--- src/components/StationTimetables.tsx
+++ src/components/StationTimetables.tsx
@@ -1,6 +1,8 @@
-import React, { VFC } from 'react';
+import React, { ChangeEvent, useState, VFC } from 'react';
 
 // material-ui
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 
 // models
@@ -12,20 +14,50 @@ import { pickByOperationalDay } from '~/presenters/TimetablePresenter';
 // components
 import TimetableComponent from '~/components/Timetable';
 
+// containers
+import Clock from '~/containers/ClockContainer';
+
 type Props = { station: string; timetables: Timetable[] };
 type VFCwithProps = VFC<Props>;
 
 const StationTimetables: VFCwithProps = ({ station, timetables }) => {
   const [weekdayTimetable, holidayTimetable] = pickByOperationalDay(timetables);
+  const { currentTime } = Clock.useContainer();
+  const selectedTimetable = [weekdayTimetable, holidayTimetable].find((t) =>
+    t.isInServiceDay(currentTime)
+  );
+
+  const [value, setValue] = useState(selectedTimetable?.id);
+  const handleChange = (
+    _: ChangeEvent<Record<string, never>>,
+    newValue: string
+  ) => {
+    setValue(newValue);
+  };
+
   return (
-    <>
-      <Typography variant="h5" component="h3">
-        {station}
-        <Typography variant="caption">発</Typography>
-      </Typography>
-      <TimetableComponent timetable={weekdayTimetable} />
-      <TimetableComponent timetable={holidayTimetable} />
-    </>
+    <article>
+      <div>
+        <Typography variant="h5" component="h3">
+          {station}
+          <Typography variant="caption">発</Typography>
+        </Typography>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label={`${station}'s Weekday and Holiday Timetable`}
+        >
+          <Tab label={weekdayTimetable.label} value={weekdayTimetable.id} />
+          <Tab label={holidayTimetable.label} value={holidayTimetable.id} />
+        </Tabs>
+      </div>
+      <div hidden={value !== weekdayTimetable.id}>
+        <TimetableComponent timetable={weekdayTimetable} />
+      </div>
+      <div hidden={value !== holidayTimetable.id}>
+        <TimetableComponent timetable={holidayTimetable} />
+      </div>
+    </article>
   );
 };
 
